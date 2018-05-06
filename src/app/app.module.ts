f596d62607090ca3392197a84ef71229f512d7be
Francisco <fobando@belizebank.com>
2018-05-05 19:53:14 -0600

    Assignment Initial Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SayLightComponent } from './components/say-light/say-light.component';
+import { SayDynamicBoldComponent } from './components/say-dynamic-bold/say-dynamic-bold.component';
 
+const appRoutes: Routes = [
+  { path: 'sayLight', component: SayLightComponent },
+  { path: 'sayBold', component: SayDynamicBoldComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SayLightComponent,
+    SayDynamicBoldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/say-dynamic-bold/say-dynamic-bold.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-say-dynamic-bold',
+  templateUrl: './say-dynamic-bold.component.html',
+  styleUrls: ['./say-dynamic-bold.component.css']
+})
+export class SayDynamicBoldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
